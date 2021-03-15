85abf0d73881efc364d84f9d32af16eb9e352f3d
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-15 12:07:55 +0700

    FIX BUG:Cannot set headers && Update posts date

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
+/** @format */
+
 // import express framework from node_modules
-const express = require("express");
+const express = require('express');
 
 // Creating express object(initializ express)
 const app = express();
 
 // import cors from node_modules (Using cors)
-const cors = require("cors");
-
-// Defining port number
-const port = 3000;
+const cors = require('cors');
 
 // determine which domain can access the website
 app.use(cors());
 
+// Defining port number
+const port = 3000;
+
 // Requiring models
-const post = require("./api/models/posts");
+const post = require('./api/models/posts');
 const postData = new post();
 
-const http = require("http");
-const url = require("url");
+// Requiring http and url
+const http = require('http');
+const url = require('url');
 
 // Function to serve all static files inside public directory.
-app.use("/static", express.static("public"));
+app.use('/static', express.static('public'));
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send("Hell, my friends Nice to Meet You welcome to My blog Post API");
+// API Endpoint
+app.get('/', (req, res) => {
+	res.status(200).send(
+		'Hello, my friends Nice to Meet You welcome to My blog Post API',
+	);
 });
 
-app.get("/api/posts/:postId", (req, res) => {
-  const postId = req.params.postId;
-  const foundPost = postData.getIndividualBlog(postId);
-  if (foundPost) res.status(200).send({ foundPost });
-  res.status(404).send("Not Found");
+// API Endpoint to return one post based on id
+app.get('/api/posts/:postId', (req, res) => {
+	const postId = req.params.postId;
+	const foundPost = postData.getIndividualBlog(postId);
+	if (foundPost) {
+		res.status(200).send({ foundPost });
+	} else {
+		res.status(404).send('Not Found');
+	}
 });
 
-app.get("/api/posts", (req, res) => {
-  res.status(200).send(postData.get());
+// API Endpoint to return all Posts
+app.get('/api/posts', (req, res) => {
+	return res.status(200).send(postData.get());
 });
 
 // Server setup
 app.listen(port, () =>
-  console.log(`Hello World app is listening on port ${port} !`)
+	console.log(`Hello World app is listening on port ${port} !`),
 );
