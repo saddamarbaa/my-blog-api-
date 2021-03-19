c0f92f1280a597d527dac5c8ae22aef4a5b696cd
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-19 08:53:46 +0700

    Uploading images to NodeJS API

--- API/models/posts.js
+++ API/models/posts.js
@@ -1,42 +1,44 @@
+/** @format */
+
 const fs = require("fs");
 const { posix } = require("path");
 const PATH = "./data.json";
 
 class Post {
-  constructor() {}
+	constructor() {}
 
-  get() {
-    // get Posts
-    return this.readData();
-  }
+	get() {
+		// get Posts
+		return this.readData();
+	}
 
-  getIndividualBlog(postId) {
-    // Get one Blog Post bae on Id
-    const posts = this.readData();
-    const foundPost = posts.find((post) => post.id === postId);
-    return foundPost;
-  }
+	getIndividualBlog(postId) {
+		// Get one Blog Post bae on Id
+		const posts = this.readData();
+		const foundPost = posts.find((post) => post.id === postId);
+		return foundPost;
+	}
 
-  addNewPost(newPost) {
-    // add new given post
-    const currentPosts = this.readData();
-    currentPosts.unshift(newPost);
-    console.log(currentPosts);
-    this.storeData(currentPosts);
-  }
+	addNewPost(newPost) {
+		// add new given post
+		const currentPosts = this.readData();
+		currentPosts.unshift(newPost);
+		console.log(currentPosts);
+		this.storeData(currentPosts);
+	}
 
-  readData() {
-    // read JSON data
-    const rawdata = fs.readFileSync(PATH);
-    const posts = JSON.parse(rawdata);
-    return (module.exports = posts);
-  }
+	readData() {
+		// read JSON data
+		const rawdata = fs.readFileSync(PATH);
+		const posts = JSON.parse(rawdata);
+		return (module.exports = posts);
+	}
 
-  storeData(rawData) {
-    // (store) write data to JSON file
-    let data = JSON.stringify(rawData);
-    fs.writeFileSync(PATH, data);
-  }
+	storeData(rawData) {
+		// (store) write data to JSON file
+		let data = JSON.stringify(rawData);
+		fs.writeFileSync(PATH, data);
+	}
 }
 
 module.exports = Post;
--- app.js
+++ app.js
@@ -1,37 +1,73 @@
 /** @format */
-// new this latter
+// vew this latter
+// https://stackabuse.com/handling-file-uploads-in-node-js-with-expres-and-multer/
 // https://lo-victoria.com/build-rest-api-with-nodejs-upload-files-mongodb
+// https://www.npmjs.com/package/multer
 
 // import express framework from node_modules
 const express = require("express");
 
+// Requiring http and url
+const http = require("http");
+const url = require("url");
+
 // Create a new instance of express (initializ express)
 const app = express();
+
+// import multer from node_modules
+const multer = require("multer");
+
+// Set Storage Engine
+// Configuring and validating the upload
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/uploads");
+	},
+	filename: (req, file, cb) => {
+		cb(
+			null,
+			`${file.fieldname}-${Date.now()}${getImageExtension(file.mimetype)}`,
+		);
+	},
+});
+
+// Check Image Extension
+const getImageExtension = (mimetype) => {
+	switch (mimetype) {
+		case "image/png":
+			return ".png";
+		case "image/jpeg":
+			return ".jpeg";
+		case "image/webp":
+			return ".webp";
+		default:
+			break;
+	}
+};
+
+// initialize upload variable
+const upload = multer({ storage: storage });
 
 // import cors from node_modules (Using cors)
 const cors = require("cors");
 
-// parses incoming requests with JSON payloads
-app.use(express.json());
-
-// Middleware
-
 // determine which domain can access the website
 app.use(cors());
 
-// Function to serve all static files inside public directory.
-app.use("/static", express.static("public"));
+// Requiring models
+const post = require("./api/models/posts");
+const { groupEnd } = require("console");
+const postData = new post();
+
+// parses incoming requests with JSON payloads
+app.use(express.json());
 
 // Defining port number
 const port = 3000;
 
-// Requiring models
-const post = require("./api/models/posts");
-const postData = new post();
-
-// Requiring http and url
-const http = require("http");
-const url = require("url");
+// Public file
+// serve all static files inside public directory.
+app.use("/static", express.static("public"));
 
 // API Endpoint
 app.get("/", (req, res) => {
@@ -54,6 +90,20 @@ app.get("/api/posts/:postId", (req, res) => {
 // API Endpoint to return all Posts
 app.get("/api/posts", (req, res) => {
 	return res.status(200).send(postData.get());
+});
+
+//  API Endpoint to add new Post
+//  upload Single file(image)
+app.post("/api/posts", upload.single("post_image"), (req, res) => {
+	const newPost = {
+		id: `${Date.now()}`,
+		title: req.body.title,
+		content: req.body.content,
+		post_image: `uploads/${req.file.filename}`,
+		added_date: `${Date.now()}`,
+	};
+	postData.addNewPost(newPost);
+	return res.status(201).send(newPost);
 });
 
 // Server setup
