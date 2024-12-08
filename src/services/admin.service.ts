0fad5bb32bc5af79f36e3c1da7bfc0b6ffce4183
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2024-12-08 12:37:05 -0800

    feat(api):add admin create post api

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -452,3 +452,73 @@ export const adminGetPostService = async (req: AuthenticatedRequestBody<IUser>, 
     return next(InternalServerError);
   }
 };
+
+export const adminCreatePostService = async (
+  req: AuthenticatedRequestBody<IPost>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { title, description, category, photoUrl } = req.body;
+
+  // Check if either an image file or photoUrl is provided
+  if (!req.file && !photoUrl) {
+    return next(createHttpError(422, `Either an image file must be uploaded or a photo URL must be provided`));
+  }
+
+  try {
+    let cloudinaryResult;
+    if (req.file?.filename) {
+      const localFilePath = `${process.env.PWD}/public/uploads/posts/${req.file?.filename}`;
+      cloudinaryResult = await cloudinary.uploader.upload(localFilePath, {
+        folder: 'posts'
+      });
+
+      // Remove file from local uploads folder
+      deleteFile(localFilePath);
+    }
+
+    // If no image file, use the provided photo URL
+    const photo = cloudinaryResult?.secure_url || photoUrl;
+
+    const postData = new Post({
+      title,
+      description,
+      category: category?.toLocaleLowerCase(),
+      photoUrl: photo,
+      cloudinary_id: cloudinaryResult?.public_id,
+      author: req?.user?._id || ''
+    });
+
+    const createdPost = await Post.create(postData);
+
+    const data = {
+      post: {
+        ...createdPost._doc,
+        author: undefined,
+        creator: req?.user
+      },
+      request: {
+        type: 'Get',
+        description: 'Get all posts',
+        url: `${process.env.API_URL}/api/${process.env.API_VERSION}/admin/posts`
+      }
+    };
+
+    return res.status(201).send(
+      customResponse<typeof data>({
+        success: true,
+        error: false,
+        message: `Successfully added new post`,
+        status: 201,
+        data
+      })
+    );
+  } catch (error) {
+    // Remove file from local uploads folder
+    if (req.file?.filename) {
+      const localFilePath = `${process.env.PWD}/public/uploads/posts/${req.file?.filename}`;
+      deleteFile(localFilePath);
+    }
+    return next(InternalServerError);
+  }
+};
