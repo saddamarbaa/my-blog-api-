254a528a2cc2b79c5bcd25dbc2bfbad3e7ba7172
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2025-01-10 16:20:07 -0800

    chore: enhance customResponse with default values for consistency

--- src/utils/customResponse.ts
+++ src/utils/customResponse.ts
@@ -1,12 +1,19 @@
 import { ResponseT } from '@src/interfaces';
 
-export const customResponse = <T>({ data, success, error, message, status }: ResponseT<T>) => {
+export const customResponse = <T>({
+  data = null,
+  success = false,
+  error = true,
+  message = 'Internal Server Error',
+  status = 500
+}: ResponseT<T>) => {
   return {
+    data,
     success,
     error,
     message,
     status,
-    data
+    stack: process.env.NODE_ENV === 'production' ? undefined : new Error().stack
   };
 };
 
